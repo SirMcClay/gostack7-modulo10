60cfbf1e986450b5894ae89af14a43f0c8ec396c
sirmcclay <mcclay.design@gmail.com>
2020-06-03 12:19:00 -0300

    Select Providers List - Making

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
 export default function SelectProvider() {
-  return <Background />;
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  function providerImg(data) {
+    if (!data.avatar) {
+      return `https://api.adorable.io/avatar/50/${data.name}.png`;
+    }
+
+    return __DEV__
+      ? `http://192.168.1.135:3333/files/${data.avatar.path}`
+      : data.avatar.url;
+  }
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider>
+              <Avatar
+                source={{
+                  uri: providerImg(provider),
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 /* SelectProvider.propTypes = {
---
+++ src/pages/New/SelectProvider/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView``;
+
+export const ProvidersList = styled.FlatList``;
+
+export const Provider = styled.View``;
+
+export const Avatar = styled.Image``;
+
+export const Name = styled.Text``;

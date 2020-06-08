6c409406c9e50fa5122dd1d111296b690a8fca0f
sirmcclay <mcclay.design@gmail.com>
2020-06-08 12:42:31 -0300

    DateInput codeing pt2

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
 import { Container } from './styles';
 
 export default function SelectDateTime() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <Background>
       <Container>
-        <DateInput />
+        <DateInput date={date} onChange={setDate} />
       </Container>
     </Background>
   );

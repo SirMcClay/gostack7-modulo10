110c998f4dc929eabb01f2177eaf0f3a3031c022
sirmcclay <mcclay.design@gmail.com>
2020-06-11 20:18:00 -0300

    Appointment confirm - pt1

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function Confirm() {
-  return <View />;
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation, route }) {
+  const { provider, time } = route.params;
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  function providerImg(data) {
+    if (!data.avatar) {
+      return `https://api.adorable.io/avatar/50/${data.name}.png`;
+    }
+
+    return __DEV__
+      ? `http://192.168.1.135:3333/files/${data.avatar.path}`
+      : data.avatar.url;
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: providerImg(provider),
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={() => {}}>Confirmar agendamento</SubmitButton>
+      </Container>
+    </Background>
+  );
 }
